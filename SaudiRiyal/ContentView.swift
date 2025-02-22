2543c5a2b1a7233830b87a308ad2a59759d2d020
Raouf <mabdulraouf@ipmagix.com>
2025-02-22 11:12:21 +0200

    Initial Commit

---
+++ SaudiRiyal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SaudiRiyal
+//
+//  Created by Raouf on 22/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SaudiRiyal/SaudiRiyalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaudiRiyalApp.swift
+//  SaudiRiyal
+//
+//  Created by Raouf on 22/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SaudiRiyalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
